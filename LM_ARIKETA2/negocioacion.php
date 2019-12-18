e51059c2af4fcac8dd4a5cc98ee2fb82a6079ddc
AS3-1-11 <AS3-1-11@uni.lan>
2019-12-18 08:57:06 +0100

    ariketa formulario

---
+++ LM_ARIKETA2/negocioacion.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_GET["botones"]))
+    echo $_GET["botones"];
+echo "<br>";
+if (isset($_GET["gender"]))
+    echo $_GET["gender"];
+echo "<br>"
+if (isset($_GET["edad"]))
+    echo $_GET["edad"];
+
