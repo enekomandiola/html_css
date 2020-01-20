7357204cbe683a00bf3d0d7062391e815e98ed76
AS3-1-11 <AS3-1-11@uni.lan>
2020-01-20 09:13:18 +0100

    calculadora

---
+++ LM_ARIKETA2/javascript/js/calculadora.js
@@ -0,0 +1,23 @@
+function suma(){
+    //suma
+    document.getElementById('numero1').value;
+    x=document.getElementById('numero1').value;
+    document.getElementById('numero2').value;
+    y=document.getElementById('numero2').value;
+    document.getElementById('resultado').x + y;
+}
+
+function resta() {
+    //resta
+    document.getElementById('numero1').value;
+    x=document.getElementById('numero1').value;
+    document.getElementById('numero2').value;
+    y=document.getElementById('numero2').value;
+    x - y.resta;
+}
+
+function cls() {
+    //cls
+    document.get
+
+}
