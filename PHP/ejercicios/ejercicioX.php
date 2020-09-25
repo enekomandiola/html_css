3b9cc9504afd9ed3bcf9caa692ecb24093daaa97
Mandiola Achabal <mandiola.eneko@uni.lan>
2020-09-25 14:08:07 +0200

    ejerciciophp1

---
+++ PHP/ejercicios/ejercicioX.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Ejemplo de ejercicio sin formulario - ejemplo-sf-1.php
+ *
+ * @author ENEKO
+ *
+ */
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>
+    Ejemplo de ejercicio sin formulario.
+    Ejemplos.
+    Eneko Mandiola
+  </title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/estiloejercicio.css" title="Color">
+</head>
+
+<body>
+  <h1>Ejemplo de ejercicio sin formulario 1</h1>
+
+  <p>Actualice la página para mostrar dos nuevos colores.</p>
+
+  <table>
+    <tbody>
+<?php
+
+print "  <p class=\"aviso\">Ejercicio incompleto</p>\n";
+
+?>
+    </tbody>
+  </table>
+
+  <footer>
+    <p>Eneko Mandiola</p>
+  </footer>
+</body>
+</html>
