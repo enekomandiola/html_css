262d0b1b3304baf30484e797b1ea9bea8f07d62a
Mandiola Achabal <mandiola.eneko@uni.lan>
2020-10-22 14:19:56 +0200

    cambiios2

---
+++ PHP/ejercicios_form/formulario4.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>FORMULARIO1</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <h1>DATOS PERSONALES</h1>
+<form action="respuesta4.php" method=POST>
+
+<label for="nombre">Nombre</label><input type="text" name="nombre">
+
+<label for="apellido">Apellido</label><input type="text" name="apellido">
+
+
+<input type="submit" name="enviar" value ="enviar">
+</form>
+
+
+</form>
+</body>
+</html>
---
+++ PHP/ejercicios_form/respuesta4.php
@@ -0,0 +1,19 @@
+<?php
+    print "<pre>";
+    print_r($_REQUEST);
+    PRINT "<pre>";
+
+    $nombre = $_REQUEST['nombre'];
+    $apellido = $_REQUEST['apellido'];
+
+
+    if(($nombre == "") && ($apellido == ""))
+    {
+        print "NO HAS INTRODUCIDO UN NOMBRE.";
+    }
+    else
+    {
+        print "<h1>RESPUESTA</h1>";
+        print "<p style='width:100px'> $nombre $apellido</p>";
+    }
+?>
